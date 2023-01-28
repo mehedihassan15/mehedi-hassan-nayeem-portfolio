d2c90efb0370c3a7ed574f78b72d1ec33d356522
Mehedi Hassan <github.mehedihassan.0955.0602@gmail.com>
2023-01-29 05:13:32 +0600

    Portfolio of Mehedi Hassan Nayeem
    Portfolio of Mehedi Hassan Nayeem

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $(".about-info-inner").typed({
+        strings: [
+            "Hi! I'm <span>Mehedi Hassan Nayeem</span>, a software engineer who lives in Dhaka, Bangladesh. I am obtaining my bachelor's degree from Roehampton University with one of their ATP partner Lithan Academy. Though I am a fresher and have yet to gain experience, I have designed more than <span>450+ websites</span> till now and <span>five full-fledged</span> websites; using various platforms such as the Job management system – ‘ABC Jobs Portal,’ the Car management system – ‘ABC Cars,’ the learning management system – ‘ABC Learning Center,’ Blogging Applications, Static websites like blogs, portfolios, Restaurant projects, etcetera. Since childhood, my dream has been to be a software engineer. I joined the program to develop myself as an engineer. I want to work hard and achieve a good position in this field so my parents can be proud of me someday. Thank you so much for your precious time visiting my portfolio.",
+        ],
+        typeSpeed: 7,
+        contentType: "html",
+    });
+  }, 100);
